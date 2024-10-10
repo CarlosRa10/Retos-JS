173aa128626c79b3a6d79c126cb5d2c2169d4e8f
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-10-10 16:58:01 -0400

    commit

---
+++ Reto_2.js
@@ -0,0 +1,19 @@
+function manufacture(gifts, materials) {
+    
+   
+}
+
+
+
+// Ejemplos de prueba
+const gifts1 = ['tren', 'oso', 'pelota'];
+const materials1 = 'tronesa';
+console.log(manufacture(gifts1, materials1)); // ["tren", "oso"]
+
+const gifts2 = ['juego', 'puzzle'];
+const materials2 = 'jlepuz';
+console.log(manufacture(gifts2, materials2)); // ["puzzle"]
+
+const gifts3 = ['libro', 'ps5'];
+const materials3 = 'psli';
+console.log(manufacture(gifts3, materials3)); // []
